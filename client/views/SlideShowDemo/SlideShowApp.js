e2a47a63450c079c60d26df1ec4bd024ecb718c4
mihnor <go@wesense.com.br>
2014-08-17 21:34:36 -0300

    lighbox nao funcionando - corrigir varias coisas

--- client/views/SlideShowDemo/SlideShowApp.js
+++ client/views/SlideShowDemo/SlideShowApp.js
@@ -1,18 +1,31 @@
 /**
  * Created by cleliodpaula on 16/8/14.
  */
+
+var currentIndex = 0;
+
+
 var Utility = require('famous/utilities/Utility');
 Utility.loadURL(SlideData.getUrl(), initApp);
+//currentIndex = 1;
+
+
+
 
 function initApp(data) {
 
+//    alert("hohoho");
     // parses out reponse data and retrieves array of urls
     data = SlideData.parse(data);
+//        alert(data.length);
+//    Session.setDefault('dataApp', data);
+    Session.setDefault('currentIndex',0);
 
-    Session.setDefault('dataApp', data);
-
-    for(var i; i< Session.get('dataApp').lenght ; i++){
-        Pics.insert({url: Session.get('dataApp')[i]})
+    if(Pics.findOne() == null) {
+        for (var i = 0; i < data.length; i++) {
+            Pics.insert({url: data[i], id: i});
+        console.log("inserindo: " + i);
+        }
     }
 }
 
@@ -22,6 +35,15 @@ function initApp(data) {
 Template.AppView.getData = function() { return Session.get('dataApp') };
 Template.SlideshowView.getData = function() { return Session.get('dataApp') };
 
+
+
+
 //Reactivity
 Session.setDefault('defaultImage', 'https://lh4.googleusercontent.com/-HbYp2q1BZfQ/U3LXxmWoy7I/AAAAAAAAAJk/VqI5bGooDaA/s1178-no/1.jpg');
-Template.SlideshowView.getImg = function() { return Session.get('defaultImage'); };
+//Template.SlideshowView.getImg = function() { return Session.get('defaultImage'); };
+
+Template.SlideshowView.getIndex = function() {
+//    return Session.get('dataApp')[Session.get('currentIndex')];
+
+    return Pics.findOne({id: currentIndex}).url;
+};
--- client/views/SlideShowDemo/SlideShowView.js
+++ client/views/SlideShowDemo/SlideShowView.js
@@ -5,14 +5,30 @@
 
 famousCmp.ready(function(require) {
     famousCmp.registerView('LightBox',  famous.views.Lightbox);
-
+    famousCmp.registerView('ContainerSurface', famous.surfaces.ContainerSurface);
 });
+
+
+Template.SlideshowView.showCurrentSlide = function() {
+    var slide = Session.get('currentIndex');
+//    this.lightbox.show(slide);
+};
 
 Template.SlideshowView.rendered = function() {
     doo = famousCmp.dataFromTemplate(this);
 
 };
 
+Template.SlideshowView.pics = function() {
+    return Pics.find({}, {sort: {url: 1}, limit: 8});
+}
+Template.SlideshowView.items = function() {
+    return Items.find({}, {sort: {name: 1}, limit: 8});
+}
+
+Template.AppView.items = function() {
+    return Items.find({}, {sort: {name: 1}, limit: 8});
+}
 
 
 Template.Lightbox.rendered = function() {
